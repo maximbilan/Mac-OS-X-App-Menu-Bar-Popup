836bc7f316989cdaad716d836c213076dec8f7bf
Maxim Bilan <maximb.mail@gmail.com>
2015-10-21 20:04:14 +0300

    Added event monitor class

---
+++ Popup/Popup/EventMonitor.swift
@@ -0,0 +1,36 @@
+//
+//  EventMonitor.swift
+//  uah
+//
+//  Created by Maxim on 10/4/15.
+//  Copyright © 2015 Maxim Bilan. All rights reserved.
+//
+
+import Cocoa
+
+public class EventMonitor {
+	
+	private var monitor: AnyObject?
+	private let mask: NSEventMask
+	private let handler: NSEvent? -> ()
+	
+	public init(mask: NSEventMask, handler: NSEvent? -> ()) {
+		self.mask = mask
+		self.handler = handler
+	}
+	
+	deinit {
+		stop()
+	}
+	
+	public func start() {
+		monitor = NSEvent.addGlobalMonitorForEventsMatchingMask(mask, handler: handler)
+	}
+	
+	public func stop() {
+		if monitor != nil {
+			NSEvent.removeMonitor(monitor!)
+			monitor = nil
+		}
+	}
+}
