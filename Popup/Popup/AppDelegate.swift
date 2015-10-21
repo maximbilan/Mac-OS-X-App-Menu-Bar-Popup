c2d5bb2a2275a2a4547f6d678a170675deacd5a9
Maxim Bilan <maximb.mail@gmail.com>
2015-10-21 20:01:47 +0300

    Added project template

---
+++ Popup/Popup/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Popup
+//
+//  Created by Maxim on 10/21/15.
+//  Copyright © 2015 Maxim. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Popup/Popup/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Popup
+//
+//  Created by Maxim on 10/21/15.
+//  Copyright © 2015 Maxim. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
